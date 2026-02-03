3f549dc1935bfd05eacef08efbdc10fc2a864db0
Associação de Moradores do Villas do Arraial <150068839+amovavillas@users.noreply.github.com>
2026-02-03 10:30:17 -0300

    Update watercalculator.js atualizar valores

--- ts/watercalculator.js
+++ ts/watercalculator.js
@@ -1,31 +1,31 @@
 function calculateConsumption(input) {
     var consumption;
     if (input >= 0 && input <= 6) {
-        consumption = 41.18;
+        consumption = 43,23;
     }
     else if (input > 6 && input <= 10) {
-        consumption = (input - 6) * 1.63 + 41.18;
+        consumption = (input - 6) * 1.71 + 43,23;
     }
     else if (input > 10 && input <= 15) {
-        consumption = (input - 10) * 11.53 + 47.7;
+        consumption = (input - 10) * 12,10 + 50,07;
     }
     else if (input > 15 && input <= 20) {
-        consumption = (input - 15) * 12.34 + 105.35;
+        consumption = (input - 15) * 12,95 + 114,82;
     }
     else if (input > 20 && input <= 25) {
-        consumption = (input - 20) * 13.87 + 167.05;
+        consumption = (input - 20) * 14,56 + 187,62;
     }
     else if (input > 25 && input <= 30) {
-        consumption = (input - 25) * 15.47 + 236.40;
+        consumption = (input - 25) * 16,24 + 268,82;
     }
     else if (input > 30 && input <= 40) {
-        consumption = (input - 30) * 17.00 + 313.75;
+        consumption = (input - 30) * 17,85 + 447,32;
     }
     else if (input > 40 && input <= 50) {
-        consumption = (input - 40) * 18.65 + 483.75;
+        consumption = (input - 40) * 19,58 + 643,12;
     }
     else {
-        consumption = (input - 50) * 22.44 + 670.25;
+        consumption = (input - 50) * 23,56 + 838,92;
     }
     return consumption.toFixed(2);
 }
